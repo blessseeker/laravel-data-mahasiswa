ae678841a834cc0e132c387eba0582a83493bc7f
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-30 10:30:45 +0700

    menambahkan old input

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -12,28 +12,28 @@
                     @csrf
                     <div class="form-group">
                       <label for="nama">Nama</label>
-                      <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama">
+                    <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" value="{{old('nama')}}">
                       @error('nama')
                           <div class="invalid-feedback">{{$message}}</div>
                       @enderror
                     </div>
                     <div class="form-group">
                       <label for="nrp">NRP</label>
-                      <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp">
+                      <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp" value="{{old('nrp')}}">
                       @error('nrp')
                           <div class="invalid-feedback">{{$message}}</div>
                       @enderror
                     </div>
                     <div class="form-group">
                       <label for="email">Email</label>
-                      <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email">
+                      <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{old('email')}}">
                       @error('email')
                           <div class="invalid-feedback">{{$message}}</div>
                       @enderror
                     </div>
                     <div class="form-group">
                       <label for="jurusan">Jurusan</label>
-                      <input type="text" class="form-control @error('jurusan') is-invalid @enderror" id="jurusan" name="jurusan">
+                      <input type="text" class="form-control @error('jurusan') is-invalid @enderror" id="jurusan" name="jurusan" value="{{old('jurusan')}}">
                       @error('jurusan')
                           <div class="invalid-feedback">{{$message}}</div>
                       @enderror
